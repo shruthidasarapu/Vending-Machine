ff1eaf56388a1de52b8876dd5fb03f8edddfdfcf
shruthi <shruthi.dasarapu@gmail.com>
2022-06-16 14:37:56 +0200

    Fixed UI issues

--- src/Components/VendingMachine.js
+++ src/Components/VendingMachine.js
@@ -1,15 +1,23 @@
 import { Products } from "./Products";
 import { useState } from "react";
-import {DIVItems, DIVProducts,DIVName,DIVPrice,DIVSelect,Form,Input } from "./VendingMachine-styles";
+import {
+  DIVItems,
+  DIVProducts,
+  DIVName,
+  DIVPrice,
+  DIVSelect,
+  Form,
+  Input,
+} from "./VendingMachine-styles";
 
 export const VendingMachine = () => {
   const [coins, setCoins] = useState(0);
   const [sumcoins, setSumcoins] = useState([]);
   const [selectedItem, setSelectedItem] = useState({});
+  const inputCoins = [20, 10, 5, 2, 1];
+  let total = sumcoins.reduce((acc, coin) => acc + coin, 0);
 
   const onSelect = (name, price) => {
-    const total = sumcoins.reduce((acc, coin) => acc + coin, 0);
-
     if (total === price) {
       setSelectedItem({ Product: name, RemainingAmount: 0 });
       return;
@@ -17,21 +25,26 @@ export const VendingMachine = () => {
       alert("Insufficient Amount");
       return;
     } else {
-      setSelectedItem({ Product: name, RemainingAmount: total - price });
-      setSumcoins([]);
-      return;
+      let remainingCoins = [];
+      let remainingAmount = total - price;
+      for (let i = 0; i < inputCoins.length; i++) {
+        if (remainingAmount <= 0) break;
+        if (remainingAmount >= inputCoins[i]) {
+          remainingAmount = remainingAmount - inputCoins[i];
+          remainingCoins.push(inputCoins[i]);
+          if (remainingAmount !== 0) continue;
+          return setSelectedItem({
+            Product: name,
+            RemainingAmount: remainingCoins,
+          });
+        }
+      }
     }
   };
   const onSubmit = (e) => {
     e.preventDefault();
 
-    if (
-      coins === 1 ||
-      coins === 2 ||
-      coins === 5 ||
-      coins === 10 ||
-      coins === 20
-    ) {
+    if (inputCoins.includes(coins)) {
       setSumcoins((coin) => [...coin, Number(coins)]);
       setCoins(0);
     } else {
@@ -39,46 +52,52 @@ export const VendingMachine = () => {
       return;
     }
   };
-  
+
   return (
     <>
       <h2>Vending Machine</h2>
-      
-      <DIVItems>
-        {Products.map(
-          (product) => (
-            <DIVProducts key = {product.id}>
-              <img src={product.imageUrl} alt = "images" />
+      <Form>
+        <Input
+          type='text'
+          paceholder='Enter coins'
+          value={coins}
+          onChange={(e) => {
+            setCoins(Number(e.target.value));
+          }}
+        />
+        <button onClick={(e) => onSubmit(e)}>submit</button>
+      </Form>
+      <div>
+        <p>Please enter coins</p>
+        {inputCoins
+          .slice()
+          .reverse()
+          .map((coin, i) => (
+            <span key={i}>
+              {coin}
+              {i < inputCoins.length - 1 ? ", " : ""}
+            </span>
+          ))}
+      </div>
+      <div>Total Amount : {total}</div>
+      <div>
+        <DIVItems>
+          {Products.map((product) => (
+            <DIVProducts key={product.id}>
+              <img src={product.imageUrl} alt='images' />
               <DIVName>{product.name}</DIVName>
               <DIVPrice> Price: {product.price}</DIVPrice>
               <DIVSelect>
                 <button
                   onClick={() => {
                     onSelect(product.name, product.price);
-                  }}
-                >
+                  }}>
                   select
                 </button>
               </DIVSelect>
             </DIVProducts>
-          )
-         
-        )}
-      </DIVItems>
-     
-      <Form>
-        <Input
-          type="text"
-          paceholder="Enter coins"
-          value={coins}
-          onChange={(e) => {
-            e.preventDefault();
-            setCoins(Number(e.target.value))}}
-        />
-        <button onClick={(e) => onSubmit(e)}>submit</button>
-      </Form>
-      <p>Please enter 1,2,5,10,20 coins only</p>
-      <div>
+          ))}
+        </DIVItems>
         {Object.keys(selectedItem).map((item, i) => {
           return (
             <div key={i}>
@@ -89,4 +108,4 @@ export const VendingMachine = () => {
       </div>
     </>
   );
-}
+};
